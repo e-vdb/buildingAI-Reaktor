3b0aad43ed9ece4d4a1f248609ab5ce8835d9181
Emeline Van der Beken <82372483+e-vdb@users.noreply.github.com>
2021-05-10 16:20:31 +0200

    Update and rename OptimizationIntermediate.py to Chapter1/pineapplesRoute.py

---
+++ Chapter1/pineapplesRoute.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 10 14:07:55 2021
+
+@author: Emeline
+
+Answer of question 1 (intermediate level)
+
+Print all routes
+"""
+
+def main():
+    portnames = ["PAN", "AMS", "CAS", "NYC", "HEL"]
+    nb=0
+    port1 = 0
+    for port2 in range(1, 5):
+        for port3 in range(1, 5):
+            for port4 in range(1, 5):
+                for port5 in range(1, 5):
+                    route = [port1, port2, port3, port4, port5]
+                    if set(route) == set(range(5)):
+                    # do not modify the print statement
+                        print(' '.join([portnames[i] for i in route]))
+                       
+
+main()
+
